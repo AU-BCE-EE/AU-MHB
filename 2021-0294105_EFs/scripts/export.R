d99e915d9446ab752467502c369dca9c7192a3d5
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-10-08 15:10:36 -0400

    Run w new acid pH, add acid red plot

--- 2021-0294105_EFs/scripts/export.R
+++ 2021-0294105_EFs/scripts/export.R
@@ -2,3 +2,7 @@
 
 # Complete EF data frame
 write.csv(dat, '../output/EF.csv', row.names = FALSE)
+
+datw <- dcast(dat, app.timing.dk + app.timing + app.mthd + incorp + t.incorp + man.source + man.name ~ acid, value.var = 'EF')
+datw$red.acid <- 100 * (1 - datw$`TRUE` / datw$`FALSE`)
+write.csv(datw, '../output/EF_acid_comp.csv', row.names = FALSE)
