5c751bb2f6618c80e55ff82b63b48080d839a362
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-10-13 13:16:40 -0400

    Fix summer merge

--- 2021-0294105_EFs/scripts/compare.R
+++ 2021-0294105_EFs/scripts/compare.R
@@ -1,6 +1,8 @@
 # Compare calculated EFs to those from 2021 EF report
 
 EFs$acid <- EFs$man.trt == 'Barn acidified'
+# To deal with e.g., "Summer, before winter rapeseed"
+EFs$app.timing[grepl('Summer', EFs$app.timing)] <- 'Summer'
 
 dat.comp <- merge(dat, EFs[EFs$decade == 2010 & EFs$man.trt != 'Field acidified', c('id', 'app.timing', 'app.mthd', 'man.source', 'acid', 'incorp', 't.incorp', 'EFp')],
       by = c('app.timing', 'app.mthd', 'man.source', 'acid', 'incorp', 't.incorp'),
