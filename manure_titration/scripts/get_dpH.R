4e4613183dbb7025c19acfe3001543b1b71106d9
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-10-25 11:54:58 -0400

    Add 2.1 kg/t dose

--- manure_titration/scripts/get_dpH.R
+++ manure_titration/scripts/get_dpH.R
@@ -1,12 +1,12 @@
 # Get interpolated values needed for acidified EFs
 
-ext <- data.frame(animal = c('Afgasset', 'Kvæg', 'Slagtesvin', 'So-/smågrise'),
-                  dose.kg.t = c(11, 3, 2.9, 2.9))
+ext <- data.frame(animal = c('Afgasset', 'Afgasset', 'Kvæg', 'Slagtesvin', 'So-/smågrise'),
+                  dose.kg.t = c(11, 2.1, 3, 2.9, 2.9))
 
 ext <- merge(ext, adat, all = FALSE)
 
-dpH.acidif <- aggregate(ext$dpH, list(animal = ext$animal), mean)
-names(dpH.acidif) <- c('animal', 'dpH.mean')
-nn <- aggregate(ext$sample, list(animal = ext$animal), function(x) length(unique(x)))
-names(nn) <- c('animal', 'n')
+dpH.acidif <- aggregate(ext$dpH, list(animal = ext$animal, dose.kg.t = ext$dose.kg.t), mean)
+names(dpH.acidif) <- c('animal', 'dose.kg.t', 'dpH.mean')
+nn <- aggregate(ext$sample, list(animal = ext$animal, dose.kg.t = ext$dose.kg.t), function(x) length(unique(x)))
+names(nn)[3] <- 'n'
 dpH.acidif <- merge(dpH.acidif, nn)
