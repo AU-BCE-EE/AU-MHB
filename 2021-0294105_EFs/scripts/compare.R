0678370e1be89ce387df6cb46e6a905fddcea4c8
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-10-25 12:36:09 -0400

    Fix merge mistake with big report EFs

--- 2021-0294105_EFs/scripts/compare.R
+++ 2021-0294105_EFs/scripts/compare.R
@@ -1,9 +1,9 @@
 # Compare calculated EFs to those from 2021 EF report
 
-EFs$acid <- EFs$man.trt == 'Barn acidified'
+EFs$acid <- EFs$man.trt == 'Field acidified'
 # To deal with e.g., "Summer, before winter rapeseed"
 EFs$app.timing[grepl('Summer', EFs$app.timing)] <- 'Summer'
 
-dat.comp <- merge(dat, EFs[EFs$decade == 2010 & EFs$man.trt != 'Field acidified', c('id', 'app.timing', 'app.mthd', 'man.source', 'acid', 'incorp', 't.incorp', 'EFp')],
+dat.comp <- merge(dat, EFs[EFs$decade == 2010 & EFs$man.trt != 'Barn acidified', c('id', 'app.timing', 'app.mthd', 'man.source', 'acid', 'incorp', 't.incorp', 'EFp')],
       by = c('app.timing', 'app.mthd', 'man.source', 'acid', 'incorp', 't.incorp'),
       suffixes = c('', '.report'), all.x = TRUE)
