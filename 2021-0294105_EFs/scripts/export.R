4bb1dee62de301fe1c068d84df5cd95b32b5792f
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-10-26 19:43:07 -0400

    Use new prepDat()/prep = TRUE option (ALFAM2 v1.2

--- 2021-0294105_EFs/scripts/export.R
+++ 2021-0294105_EFs/scripts/export.R
@@ -3,6 +3,8 @@
 # Complete EF data frame
 write.csv(dat, '../output/EF.csv', row.names = FALSE)
 
+write.csv(preds, '../output/preds.csv', row.names = FALSE)
+
 # Same but with comparison to EF report values
 write.csv(dat.comp, '../output/EF_report_comp.csv', row.names = FALSE)
 
