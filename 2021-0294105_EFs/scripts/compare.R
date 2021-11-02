925593b981384ad22555b6aeb51abcd678cd178f
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-11-02 14:37:22 -0400

    Use newest weather inputs.
    Include monthly means for rain

--- 2021-0294105_EFs/scripts/compare.R
+++ 2021-0294105_EFs/scripts/compare.R
@@ -4,9 +4,22 @@ EFs$acid <- EFs$man.trt == 'Field acidified'
 # To deal with e.g., "Summer, before winter rapeseed"
 EFs$app.timing[grepl('Summer', EFs$app.timing)] <- 'Summer'
 
+app.timing.conv <- c(March = 'Marts', April = 'April', May = 'Maj',
+                     Summer = 'Sommer', Autumn = 'Efterår')
+EFs$app.timing <- app.timing.conv[EFs$app.timing]
+
+EFs$EFp <- signif(EFs$EFp, 2)
+dat$EFp <- signif(dat$EFp, 2)
+
+man.source.conv <- c(Digestate = 'Afgasset biomasse', Cattle = 'Kvæggylle', Pig = 'Svinegylle')
+EFs$man.source <- man.source.conv[EFs$man.source]
+
 dat.comp <- merge(dat, EFs[EFs$decade == 2010 & EFs$man.trt != 'Barn acidified', c('id', 'app.timing', 'app.mthd', 'man.source', 'acid', 'incorp', 't.incorp', 'EFp')],
       by = c('app.timing', 'app.mthd', 'man.source', 'acid', 'incorp', 't.incorp'),
       suffixes = c('', '.report'), all.x = TRUE)
+
+dat.comp$different <- ifelse(dat.comp$EFp != dat.comp$EFp.report, 'Yes', 'No')
+head(dat.comp)
 
 # Compare acid 
 datw <- dcast(dat, app.timing + app.mthd + incorp + t.incorp + man.source ~ acid, value.var = 'EF')
