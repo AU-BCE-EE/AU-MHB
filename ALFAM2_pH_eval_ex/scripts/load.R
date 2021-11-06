b3ed552cea731aca20f96126c3da5b6bd0bd89f0
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-11-06 05:52:22 -0400

    Add pH response eval (external).
    Copy from ALFAM2-emis-factors after changing to par set 2

---
+++ ALFAM2_pH_eval_ex/scripts/load.R
@@ -0,0 +1,2 @@
+
+dat <- read.csv('../data/Acidification_NH3_reduction.csv', skip = 3)
---
+++ ALFAM2_pH_eval_ex/scripts/main.R
@@ -0,0 +1,11 @@
+# Evaluation of ALFAM2 model acidification effect with available final measurements
+# S. Hafner
+
+rm(list = ls())
+
+source('packages.R')
+source('load.R')
+source('missing.R')
+source('reshape.R')
+source('predict.R')
+source('plot.R')
