515e75c9abbf7740e176235d14211c82b87691d4
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-12-16 10:24:48 -0500

    Fix plots, only 5.9% DM on sel plot.

--- 2021-0294106_digestate_NH3_EFs/scripts/plot.R
+++ 2021-0294106_digestate_NH3_EFs/scripts/plot.R
@@ -1,10 +1,9 @@
 
 
-pd <- subset(dat, acid != '7.5 kg/t')
+pd <- subset(dat, acid != '7.5 kg/t' & man.dm == 5.9)
 ggplot(pd, aes(as.integer(app.timing.dk), EFp, shape = app.mthd.nm.lb, colour = app.mthd.nm.lb)) +
   geom_point(size = 3) +
   geom_line(lty = 1, alpha = 0.08) +
-  ylim(0, max(dat$EFp)) +
   labs(x = 'Udbringningsperiode', y = 'Emissionsfaktor (% af TAN)',
        shape = '', colour = '') + 
   scale_shape_manual(values = c(19, 24, 6, 20, 1, 3)) +
@@ -13,16 +12,15 @@ ggplot(pd, aes(as.integer(app.timing.dk), EFp, shape = app.mthd.nm.lb, colour = 
   theme(legend.position = 'top')
 ggsave('../plots/emis_factors_sel.png', height = 4.8, width = 6.9)
 
-ggplot(dat, aes(as.integer(app.timing.dk), EFp, shape = app.mthd.nm.lb, colour = app.mthd.nm.lb)) +
+ggplot(dat, aes(as.integer(app.timing.dk), EFp, shape = app.mthd.nm.lb, colour = factor(man.dm))) +
   geom_point(size = 3) +
   geom_line(lty = 1, alpha = 0.08) +
-  ylim(0, max(dat$EFp)) +
   labs(x = 'Udbringningsperiode', y = 'Emissionsfaktor (% af TAN)',
-       shape = '', colour = '') + 
+       shape = '', colour = 'Tørstof') + 
   scale_shape_manual(values = c(19, 24, 6, 20, 1, 3)) +
   scale_x_continuous(breaks = unique(as.integer(dat$app.timing.dk)), labels= unique(dat$app.timing.dk)) +
   theme(legend.position = 'top')
-ggsave('../plots/emis_factors.png', height = 4.8, width = 6.9)
+ggsave('../plots/emis_factors.png', height = 4.8, width = 7.2)
 
 ggplot(dat.acomp, aes(app.timing.dk, red.acid.3.4)) +
   geom_point(colour = 'red') +
