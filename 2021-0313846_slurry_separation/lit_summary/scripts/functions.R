9336c87a31d9dda89c83dfcb2e4f5bc52dbe6a23
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-01-10 11:13:50 -0500

    New summary for EF inputs

--- 2021-0313846_slurry_separation/lit_summary/scripts/functions.R
+++ 2021-0313846_slurry_separation/lit_summary/scripts/functions.R
@@ -1,10 +1,7 @@
-capwords <- function(s, strict = FALSE) {
-    cap <- function(s) paste(toupper(substring(s, 1, 1)),
-                  {s <- substring(s, 2); if(strict) tolower(s) else s},
-                             sep = "", collapse = " " )
-    sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
-}
+
 
 source('../../../functions/rounddf.R')
+source('../../../functions/capwords.R')
 source('../../../functions/rbindf.R')
+source('../../../functions/dfsumm.R')
 
--- 2021-0313846_slurry_separation/lit_summary/scripts/summ.R
+++ 2021-0313846_slurry_separation/lit_summary/scripts/summ.R
@@ -1,7 +1,51 @@
 # Summarize data
 
-dl <- melt(dat, id.vars = c('source', 'fraction'), measure.vars = c('S.DM', 'A.DM', 'TAN.sep.eff', 'S.emis.perc', 'A.emis.perc'), na.rm = TRUE)
-summ <- aggregate(dat[, c('S.DM', 'DM.sep.eff', 'TAN.sep.eff', 'S.emis.perc', 'S.emis.ini')], by = dat[, c('slurry.source', 'fraction')], 
-                  FUN = function(x) paste0(round(mean(x, na.rm = TRUE), 1), ' (', sum(!is.na(x)), ')'))
-nsumm <- aggregate(dat[, c('S.DM', 'A.DM', 'TAN.sep.eff', 'S.emis.perc', 'A.emis.perc')], by = dat[, c('slurry.source', 'fraction')], FUN = function(x) sum(!is.na(x)))
+# Long data frame for use below
+dl <- melt(dat, id.vars = c('source', 'sep.scale', 'slurry.source', 'S.exp.ID', 'A.exp.ID', 'exp.ID', 'fraction'), 
+           measure.vars = c('S.DM', 'A.DM', 'DM', 'TAN.sep.eff', 'S.emis.perc', 'A.emis.perc'), na.rm = TRUE)
+
+# DM summary ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DM summary first by study (source) and experiment or treatment etc.
+dmds1 <- aggregate(dat[, c('DM'), drop = FALSE], 
+                 by = dat[, c('source', 'sep.scale', 'slurry.source', 'exp.ID', 'fraction')], FUN = mean, na.rm = TRUE)
+# Spread out fractions across columns
+dmdw1 <- dcast(dmds1, source + sep.scale + slurry.source + exp.ID ~ fraction, value.var = 'DM')
+names(dmdw1)[5:8] <- paste0('DM.', names(dmdw1)[5:8])
+
+# Change in DM due to separation
+dmdw1$dDM.liquid <- dmdw1$DM.liquid - dmdw1$DM.raw
+dmdw1$dDM.solid <- dmdw1$DM.solid - dmdw1$DM.raw
+
+# Final summary
+dmds2 <- aggregate(dmdw1[, c('DM.raw', 'DM.liquid', 'DM.solid', 'dDM.liquid', 'dDM.solid')], 
+                 by = dmdw1[, c('sep.scale', 'slurry.source')], FUN = mean, na.rm = TRUE)
+names(dmds2)[-1:-2] <- paste0(names(dmds2)[-1:-2], '.mean')
+dmsds2 <- aggregate(dmdw1[, c('DM.raw', 'DM.liquid', 'DM.solid', 'dDM.liquid', 'dDM.solid')], 
+                 by = dmdw1[, c('sep.scale', 'slurry.source')], FUN = sd, na.rm = TRUE)
+names(dmsds2)[-1:-2] <- paste0(names(dmsds2)[-1:-2], '.sd')
+dmns2 <- aggregate(dmdw1[, c('DM.raw', 'DM.liquid', 'DM.solid', 'dDM.liquid', 'dDM.solid')], 
+                 by = dmdw1[, c('sep.scale', 'slurry.source')], FUN = function(x) sum(!is.na(x)))
+names(dmns2)[-1:-2] <- paste0(names(dmns2)[-1:-2], '.n')
+dmds2 <- merge(dmds2, dmsds2, by = c('sep.scale', 'slurry.source'))
+dmds2 <- merge(dmds2, dmns2, by = c('sep.scale', 'slurry.source'))
+dmds2 <- dmds2[order(dmds2$sep.scale, dmds2$slurry.source), ]
+
+# Other variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# First by study (source) and experiment or treatment etc.
+ds1 <- aggregate(dat[, c('DM', 'S.DM', 'DM.sep.eff', 'TAN.sep.eff', 'S.emis.perc', 'S.emis.ini')], 
+                 by = dat[, c('source', 'sep.scale', 'slurry.source', 'exp.ID', 'fraction')], FUN = mean, na.rm = TRUE)
+
+# Final summary
+ds2 <- aggregate(ds1[, c('DM', 'S.DM', 'DM.sep.eff', 'TAN.sep.eff', 'S.emis.perc', 'S.emis.ini')], 
+                 by = ds1[, c('sep.scale', 'slurry.source', 'fraction')], FUN = mean, na.rm = TRUE)
+names(ds2)[-1:-3] <- paste0(names(ds2)[-1:-3], '.mean')
+sds2 <- aggregate(ds1[, c('DM', 'S.DM', 'DM.sep.eff', 'TAN.sep.eff', 'S.emis.perc', 'S.emis.ini')], 
+                 by = ds1[, c('sep.scale', 'slurry.source', 'fraction')], FUN = sd, na.rm = TRUE)
+names(sds2)[-1:-3] <- paste0(names(sds2)[-1:-3], '.sd')
+ns2 <- aggregate(ds1[, c('DM', 'S.DM', 'DM.sep.eff', 'TAN.sep.eff', 'S.emis.perc', 'S.emis.ini')], 
+                 by = ds1[, c('sep.scale', 'slurry.source', 'fraction')], FUN = function(x) sum(!is.na(x)))
+names(ns2)[-1:-3] <- paste0(names(ns2)[-1:-3], '.n')
+ds2 <- merge(ds2, sds2, by = c('sep.scale', 'slurry.source', 'fraction'))
+ds2 <- merge(ds2, ns2, by = c('sep.scale', 'slurry.source', 'fraction'))
+ds2 <- ds2[order(ds2$sep.scale, ds2$slurry.source, ds2$fraction), ]
 
