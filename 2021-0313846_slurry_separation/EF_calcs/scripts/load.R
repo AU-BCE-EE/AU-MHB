3afe2042c228a61fae30898f0ebfa3447e86c558
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-01-10 14:35:50 -0500

    New EF calcs with sep eff and LF DM varying

--- 2021-0313846_slurry_separation/EF_calcs/scripts/load.R
+++ 2021-0313846_slurry_separation/EF_calcs/scripts/load.R
@@ -3,3 +3,4 @@ fp <- '../inputs/inputs.xlsx'
 dat <- as.data.frame(read_xlsx(fp, sheet = 1, skip = 0))
 weather <- as.data.frame(read_xlsx(fp, sheet = 2, skip = 0))
 sep <- as.data.frame(read_xlsx(fp, sheet = 3, skip = 0))
+stor <- as.data.frame(read_xlsx(fp, sheet = 4, skip = 0))
