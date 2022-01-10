0b8c85bcff441d8b7b85a428bb71ea716ac49f1d
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-01-10 14:34:47 -0500

    Tweak calcs, update input.
    Group SEGES digestate values correctly now.

--- 2021-0313846_slurry_separation/lit_summary/scripts/summ.R
+++ 2021-0313846_slurry_separation/lit_summary/scripts/summ.R
@@ -15,15 +15,17 @@ names(dmdw1)[5:8] <- paste0('DM.', names(dmdw1)[5:8])
 # Change in DM due to separation
 dmdw1$dDM.liquid <- dmdw1$DM.liquid - dmdw1$DM.raw
 dmdw1$dDM.solid <- dmdw1$DM.solid - dmdw1$DM.raw
+dmdw1$rdDM.liquid <- 100 * dmdw1$dDM.liquid / dmdw1$DM.raw
+dmdw1$rdDM.solid <- 100 * dmdw1$dDM.solid / dmdw1$DM.raw
 
 # Final summary
-dmds2 <- aggregate(dmdw1[, c('DM.raw', 'DM.liquid', 'DM.solid', 'dDM.liquid', 'dDM.solid')], 
+dmds2 <- aggregate(dmdw1[, c('DM.raw', 'DM.liquid', 'DM.solid', 'dDM.liquid', 'dDM.solid', 'rdDM.liquid', 'rdDM.solid')], 
                  by = dmdw1[, c('sep.scale', 'slurry.source')], FUN = mean, na.rm = TRUE)
 names(dmds2)[-1:-2] <- paste0(names(dmds2)[-1:-2], '.mean')
-dmsds2 <- aggregate(dmdw1[, c('DM.raw', 'DM.liquid', 'DM.solid', 'dDM.liquid', 'dDM.solid')], 
+dmsds2 <- aggregate(dmdw1[, c('DM.raw', 'DM.liquid', 'DM.solid', 'dDM.liquid', 'dDM.solid', 'rdDM.liquid', 'rdDM.solid')], 
                  by = dmdw1[, c('sep.scale', 'slurry.source')], FUN = sd, na.rm = TRUE)
 names(dmsds2)[-1:-2] <- paste0(names(dmsds2)[-1:-2], '.sd')
-dmns2 <- aggregate(dmdw1[, c('DM.raw', 'DM.liquid', 'DM.solid', 'dDM.liquid', 'dDM.solid')], 
+dmns2 <- aggregate(dmdw1[, c('DM.raw', 'DM.liquid', 'DM.solid', 'dDM.liquid', 'dDM.solid', 'rdDM.liquid', 'rdDM.solid')], 
                  by = dmdw1[, c('sep.scale', 'slurry.source')], FUN = function(x) sum(!is.na(x)))
 names(dmns2)[-1:-2] <- paste0(names(dmns2)[-1:-2], '.n')
 dmds2 <- merge(dmds2, dmsds2, by = c('sep.scale', 'slurry.source'))
@@ -32,6 +34,9 @@ dmds2 <- dmds2[order(dmds2$sep.scale, dmds2$slurry.source), ]
 
 # Other variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # First by study (source) and experiment or treatment etc.
+##ds1 <- aggregate(dl[, c('value'), drop = FALSE], 
+##                 by = dl[, c('source', 'sep.scale', 'slurry.source', 'exp.ID', 'fraction', 'variable')], FUN = mean, na.rm = TRUE)
+
 ds1 <- aggregate(dat[, c('DM', 'S.DM', 'DM.sep.eff', 'TAN.sep.eff', 'S.emis.perc', 'S.emis.ini')], 
                  by = dat[, c('source', 'sep.scale', 'slurry.source', 'exp.ID', 'fraction')], FUN = mean, na.rm = TRUE)
 
