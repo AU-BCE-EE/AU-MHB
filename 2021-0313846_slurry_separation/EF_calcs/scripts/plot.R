8eb93bb4eeabd6678f006b7bb91b87cec5565ec0
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-01-11 12:37:51 -0500

    Add missing change from JohannaPedersen

--- 2021-0313846_slurry_separation/EF_calcs/scripts/plot.R
+++ 2021-0313846_slurry_separation/EF_calcs/scripts/plot.R
@@ -34,7 +34,7 @@ ggplot(dats, aes(app.timing.num, mid, shape = EF.type, colour = EF.type)) +
   geom_line(lty = 1, alpha = 0.08) +
   facet_wrap( ~ man.source) +
   ylim(0, max(datl$EFp.overall)) +
-  labs(x = 'Udbringningsperiode', y = 'OVERALL COMBINED Emissionsfaktor (% af TAN)', shape = '', colour = '') + 
+  labs(x = 'Udbringningsperiode', y = 'Samlet emissionsfaktor (% af TAN)', shape = '', colour = '') + 
   scale_shape_manual(values = c(19, 24, 6, 20, 1)) +
   scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), labels= unique(dat$app.timing)) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = 'top')
