bef6f533b8892d50acd31482b195910920817f16
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-01-11 11:26:07 -0500

    Group by set, add small table export

--- 2021-0313846_slurry_separation/lit_summary/scripts/export.R
+++ 2021-0313846_slurry_separation/lit_summary/scripts/export.R
@@ -1,3 +1,5 @@
 
 write.csv(dmds2, '../output/DM_summary.csv', row.names = FALSE)
 write.csv(ds2, '../output/var_summary.csv', row.names = FALSE)
+write.csv(dmtab, '../output/DM_table.csv', row.names = FALSE)
+write.csv(setab, '../output/sep_eff_table.csv', row.names = FALSE)
--- 2021-0313846_slurry_separation/lit_summary/scripts/summ.R
+++ 2021-0313846_slurry_separation/lit_summary/scripts/summ.R
@@ -1,15 +1,15 @@
 # Summarize data
 
 # Long data frame for use below
-dl <- melt(dat, id.vars = c('source', 'sep.scale', 'slurry.source', 'S.exp.ID', 'A.exp.ID', 'exp.ID', 'fraction'), 
+dl <- melt(dat, id.vars = c('source', 'sep.scale', 'slurry.source', 'S.exp.ID', 'A.exp.ID', 'set', 'fraction'), 
            measure.vars = c('S.DM', 'A.DM', 'DM', 'TAN.sep.eff', 'S.emis.perc', 'A.emis.perc'), na.rm = TRUE)
 
 # DM summary ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # DM summary first by study (source) and experiment or treatment etc.
 dmds1 <- aggregate(dat[, c('DM'), drop = FALSE], 
-                 by = dat[, c('source', 'sep.scale', 'slurry.source', 'exp.ID', 'fraction')], FUN = mean, na.rm = TRUE)
+                 by = dat[, c('source', 'sep.scale', 'slurry.source', 'set', 'fraction')], FUN = mean, na.rm = TRUE)
 # Spread out fractions across columns
-dmdw1 <- dcast(dmds1, source + sep.scale + slurry.source + exp.ID ~ fraction, value.var = 'DM')
+dmdw1 <- dcast(dmds1, source + sep.scale + slurry.source + set ~ fraction, value.var = 'DM')
 names(dmdw1)[5:8] <- paste0('DM.', names(dmdw1)[5:8])
 
 # Change in DM due to separation
@@ -32,13 +32,18 @@ dmds2 <- merge(dmds2, dmsds2, by = c('sep.scale', 'slurry.source'))
 dmds2 <- merge(dmds2, dmns2, by = c('sep.scale', 'slurry.source'))
 dmds2 <- dmds2[order(dmds2$sep.scale, dmds2$slurry.source), ]
 
+# DM table for report ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+dmtab <- dmds2[dmds2$sep.scale == 'full' & dmds2$slurry.source %in% c('cattle', 'pig', 'digestate'), 
+             c('slurry.source', 'DM.raw.mean', 'DM.liquid.mean', 'DM.solid.mean', 'rdDM.liquid.mean', 'rdDM.liquid.sd', 'rdDM.liquid.n')]
+dmtab <- rounddf(dmtab, 1)
+
 # Other variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # First by study (source) and experiment or treatment etc.
 ##ds1 <- aggregate(dl[, c('value'), drop = FALSE], 
-##                 by = dl[, c('source', 'sep.scale', 'slurry.source', 'exp.ID', 'fraction', 'variable')], FUN = mean, na.rm = TRUE)
+##                 by = dl[, c('source', 'sep.scale', 'slurry.source', 'set', 'fraction', 'variable')], FUN = mean, na.rm = TRUE)
 
 ds1 <- aggregate(dat[, c('DM', 'S.DM', 'DM.sep.eff', 'TAN.sep.eff', 'S.emis.perc', 'S.emis.ini')], 
-                 by = dat[, c('source', 'sep.scale', 'slurry.source', 'exp.ID', 'fraction')], FUN = mean, na.rm = TRUE)
+                 by = dat[, c('source', 'sep.scale', 'slurry.source', 'set', 'fraction')], FUN = mean, na.rm = TRUE)
 
 # Final summary
 ds2 <- aggregate(ds1[, c('DM', 'S.DM', 'DM.sep.eff', 'TAN.sep.eff', 'S.emis.perc', 'S.emis.ini')], 
@@ -53,4 +58,10 @@ names(ns2)[-1:-3] <- paste0(names(ns2)[-1:-3], '.n')
 ds2 <- merge(ds2, sds2, by = c('sep.scale', 'slurry.source', 'fraction'))
 ds2 <- merge(ds2, ns2, by = c('sep.scale', 'slurry.source', 'fraction'))
 ds2 <- ds2[order(ds2$sep.scale, ds2$slurry.source, ds2$fraction), ]
+
+# Sep eff table for report ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+setab <- ds2[ds2$sep.scale == 'full' & ds2$slurry.source %in% c('cattle', 'pig', 'digestate') & ds2$fraction == 'liquid', 
+             c('slurry.source', 'TAN.sep.eff.mean', 'TAN.sep.eff.sd', 'TAN.sep.eff.n')]
+setab <- rounddf(setab, 1)
+
 
