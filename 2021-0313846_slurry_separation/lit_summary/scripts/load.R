c259e8cb051ecdd283f4733ae7944ecd2534feaa
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-01-13 10:10:49 -0500

    Move lit data for use by multiple subdirs

--- 2021-0313846_slurry_separation/lit_summary/scripts/load.R
+++ 2021-0313846_slurry_separation/lit_summary/scripts/load.R
@@ -1,3 +1,3 @@
 
-dat <- as.data.frame(read_xlsx('../data/literature.xlsx', sheet = 2, skip = 2, na = 'NA'))
+dat <- as.data.frame(read_xlsx('../../lit_data/literature.xlsx', sheet = 2, skip = 2, na = 'NA'))
 EFs <- read.csv('../../EF_calcs/output/EF.csv')
