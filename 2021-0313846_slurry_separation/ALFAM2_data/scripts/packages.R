d9db06c00d7814ce6cc9105f66419c8d48d9264e
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-01-14 09:52:39 -0500

    Add package version logs

--- 2021-0313846_slurry_separation/ALFAM2_data/scripts/packages.R
+++ 2021-0313846_slurry_separation/ALFAM2_data/scripts/packages.R
@@ -1,3 +1,8 @@
 
 library(ggplot2)
 library(ALFAM2)
+
+sink('../logs/versions.txt')
+  print(sessionInfo())
+sink()
+
--- 2021-0313846_slurry_separation/lit_plot/scripts/packages.R
+++ 2021-0313846_slurry_separation/lit_plot/scripts/packages.R
@@ -2,3 +2,7 @@ library(readxl)
 library(ggplot2)
 library(dplyr)
 library(plyr)
+
+sink('../logs/versions.txt')
+  print(sessionInfo())
+sink()
