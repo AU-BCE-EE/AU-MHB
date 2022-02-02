e600cdd027aaca46db56aad082070d60b8648951
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-02-02 12:13:56 -0500

    Plot tweaks, move factor lines

--- 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/factors.R
+++ 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/factors.R
@@ -2,6 +2,12 @@
 
 datl$app.timing <- factor(datl$app.timing, levels = c('March', 'April', 'May', 'Summer', 'Autumn'))
 
+datl$app.mthd <- factor(datl$app.mthd, levels = c('Trailing hose', 'Open slot injection'))
+datl$sep <- datl$scenario != 'ref'
+datl$EF.type <- factor(paste(datl$sep, datl$incorp.sf), 
+                       levels = c('FALSE none', 'TRUE none', 'TRUE deep'),
+                       labels = c('Reference', 'Separation', 'Separation w/ SF incorporation'))
+
 
 
 
--- 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/plot.R
+++ 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/plot.R
@@ -1,9 +1,4 @@
 
-datl$app.mthd <- factor(datl$app.mthd, levels = c('Trailing hose', 'Open slot injection'))
-datl$sep <- datl$scenario != 'ref'
-datl$EF.type <- factor(paste(datl$sep, datl$incorp.sf), 
-                       levels = c('FALSE none', 'TRUE none', 'TRUE deep'),
-                       labels = c('Reference', 'Separation', 'Separation w/ SF incorporation'))
 
 
 datl$app.timing.num <- as.integer(datl$app.timing) + as.numeric(datl$EF.type)/10 - 0.2
@@ -17,5 +12,6 @@ ggplot(dd, aes(app.timing.num, EFp.overall, shape = EF.type, colour = EF.type)) 
   scale_x_continuous(breaks = unique(as.integer(datl$app.timing)), labels= unique(datl$app.timing)) +
   ylim(0, 40) +
   theme(legend.position = 'top') +
-  facet_grid(man.source ~ app.mthd) 
-ggsave('../plots/overall_emis_factors.png', height = 6, width = 5)
+  facet_grid(man.source ~ app.mthd)  +
+  labs(x = 'Application period', y = 'Overall emission factor (% of applied TAN)', shape = '', colour = '') 
+ggsave('../plots/overall_emis_factors.png', height = 6, width = 6)
