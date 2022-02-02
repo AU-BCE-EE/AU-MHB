f1b3555836597645533fd1df9b96320d3c6501f8
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-02-02 12:07:49 -0500

    Plot tweaks

--- 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/plot.R
+++ 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/plot.R
@@ -1,14 +1,21 @@
 
+datl$app.mthd <- factor(datl$app.mthd, levels = c('Trailing hose', 'Open slot injection'))
 datl$sep <- datl$scenario != 'ref'
-datl$EF.type <- interaction(datl$sep, datl$incorp.sf)
+datl$EF.type <- factor(paste(datl$sep, datl$incorp.sf), 
+                       levels = c('FALSE none', 'TRUE none', 'TRUE deep'),
+                       labels = c('Reference', 'Separation', 'Separation w/ SF incorporation'))
+
 
 datl$app.timing.num <- as.integer(datl$app.timing) + as.numeric(datl$EF.type)/10 - 0.2
 
 dd <- subset(datl, scenario %in% c('ref', 'mid'))
-ggplot(dd, aes(app.timing.num, EFp.overall, shape = incorp.sf, colour = EF.type)) +
+ggplot(dd, aes(app.timing.num, EFp.overall, shape = EF.type, colour = EF.type)) +
   geom_point() +
   geom_line() +
   geom_line(data = datl, aes(group = interaction(EF.type, app.timing))) +
+  scale_shape_manual(values = c(19, 24, 6, 20, 1)) +
+  scale_x_continuous(breaks = unique(as.integer(datl$app.timing)), labels= unique(datl$app.timing)) +
   ylim(0, 40) +
-  facet_grid(app.mthd ~ man.source) 
-ggsave('../plots/overall_emis_factors.png', height = 4.8, width = 6.9)
+  theme(legend.position = 'top') +
+  facet_grid(man.source ~ app.mthd) 
+ggsave('../plots/overall_emis_factors.png', height = 6, width = 5)
