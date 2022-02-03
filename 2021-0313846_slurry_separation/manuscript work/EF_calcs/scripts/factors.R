ea8cd59feb116c51d271b0c4d87ab730704d9bce
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-02-03 06:28:12 -0500

    Fix SF application issue.
    Changes overall EF so they now look the same as in the meom.

--- 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/factors.R
+++ 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/factors.R
@@ -2,7 +2,7 @@
 
 datl$app.timing <- factor(datl$app.timing, levels = c('March', 'April', 'May', 'Summer', 'Autumn'))
 
-datl$app.mthd <- factor(datl$app.mthd, levels = c('Trailing hose', 'Open slot injection'))
+datl$app.mthd.rl <- factor(datl$app.mthd.rl, levels = c('Trailing hose', 'Open slot injection'))
 datl$sep <- datl$scenario != 'ref'
 datl$EF.type <- factor(paste(datl$sep, datl$incorp.sf), 
                        levels = c('FALSE none', 'TRUE none', 'TRUE deep'),
