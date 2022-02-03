b3544183a4bbbfab42d9473d3600764d8713e317
Johanna Pedersen <au583430@uni.au.dk>
2022-02-03 12:53:14 +0100

    Running the code

--- 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/factors.R
+++ 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/factors.R
@@ -2,7 +2,7 @@
 
 datl$app.timing <- factor(datl$app.timing, levels = c('March', 'April', 'May', 'Summer', 'Autumn'))
 
-datl$app.mthd <- factor(datl$app.mthd, levels = c('Trailing hose', 'Open slot injection'))
+datl$app.mthd <- factor(datl$app.mthd, levels = c('Trailing hose', 'Open slot injection', 'Trailing shoe'))
 datl$sep <- datl$scenario != 'ref'
 datl$EF.type <- factor(paste(datl$sep, datl$incorp.sf), 
                        levels = c('FALSE none', 'TRUE none', 'TRUE deep'),
