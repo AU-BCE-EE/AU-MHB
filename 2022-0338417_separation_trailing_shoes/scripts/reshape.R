2bb841ba1331d5386ccd8e82e97e2991eb0903b3
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-02-08 06:39:32 -0500

    Fix sort order

--- 2022-0338417_separation_trailing_shoes/scripts/reshape.R
+++ 2022-0338417_separation_trailing_shoes/scripts/reshape.R
@@ -8,3 +8,4 @@ names(efmns)[3:4] <- paste0(names(efmns)[3:4], '.gennem')
 # Merge in means
 eftab <- merge(eftab, efmns, by = c('man.dm.dk', 'man.ph.dk'), suffixes = c('', '.gennem'))
 eftab <- rounddf(eftab, 0, func = round)
+eftab <- eftab[order(eftab$man.dm.dk, eftab$man.ph.dk), ]
