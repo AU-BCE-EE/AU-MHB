5ac52066371905be010e7dc9fadee67037bdeb6e
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-03-07 10:55:00 -0500

    Move manuscript work to new repo slurry-sep-man

--- 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/export.R
+++
@@ -1,9 +0,0 @@
-# Export results
-
-# Complete EF data frame
-write.csv(datl, '../output/EF.csv', row.names = FALSE)
-
-##write.csv(preds, '../output/preds.csv', row.names = FALSE)
-##
-##datw <- datw[order(datw$app.timing, datw$man.source), ]
-##write.csv(datw, '../output/EF_sep_comp.csv', row.names = FALSE)
