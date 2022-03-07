5aa868af236e13d6b32822d93c3421af66963ddf
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-03-07 10:55:00 -0500

    Move manuscript work to new repo slurry-sep-man

--- 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/functions.R
+++
@@ -1,4 +0,0 @@
-
-source('../../../../functions/dfcombos.R')
-source('../../../../functions/dfsumm.R')
-source('../../../../functions/rbindf.R')
--- 2021-0313846_slurry_separation/manuscript work/EF_calcs/scripts/load.R
+++
@@ -1,8 +0,0 @@
-fp <- '../inputs/inputs.xlsx'
-
-scenarios <- as.data.frame(read_xlsx(fp, sheet = 1, skip = 0))
-comp <- as.data.frame(read_xlsx(fp, sheet = 2, skip = 0))
-weather <- as.data.frame(read_xlsx(fp, sheet = 3, skip = 0))
-sep <- as.data.frame(read_xlsx(fp, sheet = 4, skip = 0))
-stor <- as.data.frame(read_xlsx(fp, sheet = 5, skip = 0))
-etime <- as.data.frame(read_xlsx(fp, sheet = 6, skip = 0))
