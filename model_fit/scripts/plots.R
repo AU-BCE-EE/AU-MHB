b3927bc3a86c25b6a4c3b581d4a29202c032e905
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-05-03 14:12:26 -0400

    Add pars01 to plot, notice missing dfs etc. for plots

--- model_fit/scripts/plots.R
+++ model_fit/scripts/plots.R
@@ -23,33 +23,33 @@ ggplot(d.pred.168, aes(country, 100 * error.er, fill = country)) +
        fill = '') 
 ggsave('../plots/error_box_set.png', height = 6, width = 6)
 
-ggplot(d.pred.168, aes(er.pred, er, colour = country, shape = set)) +
-  geom_abline(intercept = 0, slope = 1, col = 'gray45') +
-  geom_point(bg = 'gray45') +
-  #geom_line(stat = 'smooth', method = 'lm', se = FALSE, alpha = 0.6) +
-  scale_shape_manual(values = c(1, 2, 20)) +
-  facet_grid(man.source.nm ~ app.mthd.nm) +
-  labs(x = 'Calculated', 
-       y = 'Measured',
-       colour = '', shape = '') +
-  theme(legend.position = 'top') +
-  xlim(0, 0.9) +
-  guides(colour = guide_legend(nrow = 1))
-ggsave('../plots/e168_comp_set_all_sources.png', height = 4.5, width = 7)
-
-ggplot(d.pred.168.cp, aes(er.pred, er, colour = country, shape = set)) +
-  geom_abline(intercept = 0, slope = 1, col = 'gray45') +
-  geom_point(bg = 'gray45') +
-  #geom_line(stat = 'smooth', method = 'lm', se = FALSE, alpha = 0.6) +
-  scale_shape_manual(values = c(1, 2, 20)) +
-  facet_grid(man.source.nm ~ app.mthd.nm) +
-  labs(x = 'Calculated', 
-       y = 'Measured',
-       colour = '', shape = '') +
-  theme(legend.position = 'top') +
-  xlim(0, 0.9) +
-  guides(colour = guide_legend(nrow = 1))
-ggsave('../plots/e168_comp_set.png', height = 4.5, width = 7)
+#ggplot(d.pred.168, aes(er.pred, er, colour = country, shape = set)) +
+#  geom_abline(intercept = 0, slope = 1, col = 'gray45') +
+#  geom_point(bg = 'gray45') +
+#  #geom_line(stat = 'smooth', method = 'lm', se = FALSE, alpha = 0.6) +
+#  scale_shape_manual(values = c(1, 2, 20)) +
+#  facet_grid(man.source.nm ~ app.mthd.nm) +
+#  labs(x = 'Calculated', 
+#       y = 'Measured',
+#       colour = '', shape = '') +
+#  theme(legend.position = 'top') +
+#  xlim(0, 0.9) +
+#  guides(colour = guide_legend(nrow = 1))
+#ggsave('../plots/e168_comp_set_all_sources.png', height = 4.5, width = 7)
+#
+#ggplot(d.pred.168.cp, aes(er.pred, er, colour = country, shape = set)) +
+#  geom_abline(intercept = 0, slope = 1, col = 'gray45') +
+#  geom_point(bg = 'gray45') +
+#  #geom_line(stat = 'smooth', method = 'lm', se = FALSE, alpha = 0.6) +
+#  scale_shape_manual(values = c(1, 2, 20)) +
+#  facet_grid(man.source.nm ~ app.mthd.nm) +
+#  labs(x = 'Calculated', 
+#       y = 'Measured',
+#       colour = '', shape = '') +
+#  theme(legend.position = 'top') +
+#  xlim(0, 0.9) +
+#  guides(colour = guide_legend(nrow = 1))
+#ggsave('../plots/e168_comp_set.png', height = 4.5, width = 7)
 
 dd <- subset(d.pred.168, app.mthd %in% c('bc', 'bsth'))
 ggplot(dd, aes(er.pred, er, colour = digestate, shape = digestate)) +
@@ -65,31 +65,31 @@ ggplot(dd, aes(er.pred, er, colour = digestate, shape = digestate)) +
   guides(colour = guide_legend(nrow = 1))
 ggsave('../plots/e168_comp_dig.png', height = 4.5, width = 4)
 
-ggplot(d.pred.168.incorp, aes(er.pred, er, colour = country, shape = set)) +
-  geom_abline(intercept = 0, slope = 1, col = 'gray45') +
-  geom_point(bg = 'gray45') +
-  #geom_line(stat = 'smooth', method = 'lm', se = FALSE, alpha = 0.6) +
-  scale_shape_manual(values = c(1, 20)) +
-  facet_wrap(~ app.mthd) +
-  labs(x = 'Calculated', 
-       y = 'Measured',
-       colour = '', shape = '') +
-  theme(legend.position = 'top') +
-  xlim(0, 0.9) +
-  guides(colour = guide_legend(nrow = 1))
-ggsave('../plots/e168incorp_comp_set.png', height = 4.5, width = 7)
+#ggplot(d.pred.168.incorp, aes(er.pred, er, colour = country, shape = set)) +
+#  geom_abline(intercept = 0, slope = 1, col = 'gray45') +
+#  geom_point(bg = 'gray45') +
+#  #geom_line(stat = 'smooth', method = 'lm', se = FALSE, alpha = 0.6) +
+#  scale_shape_manual(values = c(1, 20)) +
+#  facet_wrap(~ app.mthd) +
+#  labs(x = 'Calculated', 
+#       y = 'Measured',
+#       colour = '', shape = '') +
+#  theme(legend.position = 'top') +
+#  xlim(0, 0.9) +
+#  guides(colour = guide_legend(nrow = 1))
+#ggsave('../plots/e168incorp_comp_set.png', height = 4.5, width = 7)
 
-ggplot(d.pred.incorp, aes(ct, er, colour = incorp, group = pmid)) +
-  geom_vline(aes(xintercept = time.incorp, colour = incorp), lty = 3) +
-  geom_line(aes(ct, er.pred), lty = 2) +
-  geom_line() +
-  facet_wrap(~ interaction(country, inst, exper), scale = 'free') +
-  labs(x = 'Time (h)', 
-       y = 'Emission (frac. applied TAN)',
-       colour = 'Incorporation') +
-  theme(legend.position = 'top') +
-  guides(colour = guide_legend(nrow = 1))
-ggsave('../plots/emis_incorp.png', height = 7, width = 12)
+#ggplot(d.pred.incorp, aes(ct, er, colour = incorp, group = pmid)) +
+#  geom_vline(aes(xintercept = time.incorp, colour = incorp), lty = 3) +
+#  geom_line(aes(ct, er.pred), lty = 2) +
+#  geom_line() +
+#  facet_wrap(~ interaction(country, inst, exper), scale = 'free') +
+#  labs(x = 'Time (h)', 
+#       y = 'Emission (frac. applied TAN)',
+#       colour = 'Incorporation') +
+#  theme(legend.position = 'top') +
+#  guides(colour = guide_legend(nrow = 1))
+#ggsave('../plots/emis_incorp.png', height = 7, width = 12)
 
 # Plot 50 random flux plots
 set.seed(124)
@@ -101,6 +101,7 @@ zero$e.pred <- 0
 dd <- rbindf(dd, zero)
 ggplot(dd, aes(ct, e.cum, colour = app.mthd, group = pmid)) +
   geom_line(aes(ct, e.pred), lty = 2) +
+  geom_line(aes(ct, e.pred1), lty = 3) +
   geom_line() +
   xlim(0, 72) +
   facet_wrap(~ interaction(country, pmid), scale = 'fixed', ncol = 5) +
