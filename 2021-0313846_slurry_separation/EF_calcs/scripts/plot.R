0e85071eb23a22bac87c2d9792445d92792f3e6a
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2023-11-21 11:55:02 +0100

    making plot for presentation

--- 2021-0313846_slurry_separation/EF_calcs/scripts/plot.R
+++ 2021-0313846_slurry_separation/EF_calcs/scripts/plot.R
@@ -45,6 +45,7 @@ ggplot(dats, aes(app.timing.num, mid, shape = EF.type, colour = EF.type)) +
   facet_wrap( ~ man.source) +
   ylim(0, max(datl$EFp.overall)) +
   labs(x = 'Udbringningsperiode', y = 'Samlet emissionsfaktor (% af TAN)', shape = '', colour = '') + 
+  scale_color_brewer(palette = 'Set1') + 
   scale_shape_manual(values = c(19, 24, 6, 20, 1)) +
   scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), labels= unique(dat$app.timing)) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = 'top')
