b4debc81aaecd549be5f67b2c13bfbe2c3cdd3f7
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2023-11-21 11:22:54 +0100

    Making a plot for a presentation

--- 2021-0313846_slurry_separation/EF_calcs/scripts/plot.R
+++ 2021-0313846_slurry_separation/EF_calcs/scripts/plot.R
@@ -32,3 +32,21 @@ ggplot(dats, aes(app.timing.num, mid, shape = EF.type, colour = EF.type)) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = 'top')
 ggsave('../plots/overall_emis_factors.png', height = 4.8, width = 6.9)
 
+
+# For JP presentation 231128
+
+dats$app.timing.num <- as.integer(dats$app.timing) + as.numeric(dats$EF.type)/10 - 0.2
+dats$highest <- pmax(dats$high, dats$high2)
+ggplot(dats, aes(app.timing.num, mid, shape = EF.type, colour = EF.type)) +
+  geom_point() +
+  theme_bw() + 
+  geom_errorbar(aes(ymin = low, ymax = highest), width = 0) +
+  geom_line(lty = 1, alpha = 0.08) +
+  facet_wrap( ~ man.source) +
+  ylim(0, max(datl$EFp.overall)) +
+  labs(x = 'Udbringningsperiode', y = 'Samlet emissionsfaktor (% af TAN)', shape = '', colour = '') + 
+  scale_shape_manual(values = c(19, 24, 6, 20, 1)) +
+  scale_x_continuous(breaks = unique(as.integer(dat$app.timing)), labels= unique(dat$app.timing)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = 'top')
+ggsave('../plots/overall_emis_factorsA.png', height = 4.8, width = 6.9)
+
