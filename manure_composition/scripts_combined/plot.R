8d2eafd4eddf2da4de127a3f7f81e946e0d3a136
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2025-03-28 07:59:38 +0100

    adding boxplots of TAN, totN, and pH

--- manure_composition/scripts_combined/plot.R
+++ manure_composition/scripts_combined/plot.R
@@ -7,3 +7,25 @@ ggplot(dd, aes(source.nm, value, fill = source.nm)) +
   labs(x = '', y = 'Tørstof (%)') +
   theme(legend.position = 'none')
 ggsave('../plots/DM_box.png', height = 4, width = 4)
+
+dd <- subset(dl, variable == 'pH')
+ggplot(dd, aes(source.nm, value, fill = source.nm)) +
+  geom_boxplot() +
+  labs(x = '', y = 'pH') +
+  theme(legend.position = 'none')
+ggsave('../plots/pH_box.png', height = 4, width = 4)
+
+dd <- subset(dl, variable == 'TN')
+ggplot(dd, aes(source.nm, value, fill = source.nm)) +
+  geom_boxplot() +
+  labs(x = '', y = 'Total N (g/kg)') +
+  theme(legend.position = 'none')
+ggsave('../plots/TotalN_box.png', height = 4, width = 4)
+
+dd <- subset(dl, variable == 'TAN')
+ggplot(dd, aes(source.nm, value, fill = source.nm)) +
+  geom_boxplot() +
+  labs(x = '', y = 'TAN (g/kg)') +
+  theme(legend.position = 'none')
+ggsave('../plots/TAN_box.png', height = 4, width = 4)
+
